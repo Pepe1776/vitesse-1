e12122e84fb2e2dba1fc63f3f83d533d9619e431
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2021-12-23 22:33:17 -0700

    blank vitesse template cleared

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,8 @@
 <script setup lang="ts">
 import { isDark, toggleDark } from '~/composables'
 
-const { t, availableLocales, locale } = useI18n()
+const { t } = useI18n()
 
-const toggleLocales = () => {
-  // change to some real logic
-  const locales = availableLocales
-  locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
-}
 </script>
 
 <template>
@@ -20,17 +15,5 @@ const toggleLocales = () => {
       <carbon-moon v-if="isDark" />
       <carbon-sun v-else />
     </button>
-
-    <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales">
-      <carbon-language />
-    </a>
-
-    <router-link class="icon-btn mx-2" to="/about" :title="t('button.about')">
-      <carbon-dicom-overlay />
-    </router-link>
-
-    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
-      <carbon-logo-github />
-    </a>
   </nav>
 </template>
